2e213974d8cc64ad27aca3ca24d4b8b0507bf5a6
Cristianangelo07 <cristian.angelo@hotmail.com>
2023-02-28 20:17:03 -0300

    Meu projeto de previsão do tempo

---
+++ scripts.js
@@ -0,0 +1,36 @@
+
+
+let chave = "364703e52c84052ebe380f0ab5d1c9d1"
+
+function colocarNaTela(dados){
+    console.log(dados)
+
+document.querySelector(".cidade").innerHTML = dados.name
+document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+document.querySelector(".descricao").innerHTML = dados.weather[0].description
+document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+document.querySelector(".umidade").innerHTML = "umidade:" + Math.floor(dados.main.humidity) + "%"
+}
+async function buscarCidade(cidade){
+let dados = await fetch( 
+    "https://api.openweathermap.org/data/2.5/weather?q=" + cidade + 
+    "&appid=" + chave + "&lang=pt_br" + "&units=metric")
+    .then( resposta => resposta.json())
+
+
+colocarNaTela(dados)
+
+}
+
+
+
+
+
+function cliqueiNoBotao(){
+    let cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+}
+
+
+
